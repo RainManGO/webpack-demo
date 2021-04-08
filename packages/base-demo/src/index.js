7fa1a6760a00315481b34eb8791d93ae0cca0775
张宇 <776210576@qq.com>
2021-04-08 20:23:41 +0800

    完成base-demo

---
+++ packages/base-demo/src/index.js
@@ -0,0 +1,16 @@
+/*
+ * @Description: 
+ * @Author: ZY
+ * @Date: 2021-04-08 11:28:20
+ * @LastEditors: ZY
+ * @LastEditTime: 2021-04-08 15:32:01
+ */
+
+import hero from "./hero.json";
+
+function packageJs(bigbang) {
+    console.log(bigbang);   
+    console.log(hero);   
+}
+
+packageJs('寒冰射手')
